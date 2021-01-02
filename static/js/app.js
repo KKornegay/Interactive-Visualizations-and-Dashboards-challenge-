a32e277c6705cf1dbf09fa7bb3487015717024d1
Kirk Kornegay <kkornegay1@gmail.com>
2021-01-02 17:28:38 -0500

    basically started over

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,40 @@
-function createcharts(sample){
-    console.log (sample)
-    
-    
-    //.filter on ID
-    
-    }
-    //build dropdown for ID
-    d3.json("samples.json").then((samples)=>{
-    console.log(samples)
-    var selection = d3.select("#selDataset")
-    samples.names.forEach((ID)=> {
-        selection.append("option").text(ID).property("value",ID)
+function plotData() {
+// Read samples.json
+    d3.json("samples.json").then (sampledata => {
+        // create variables for top 10 samples and values
+        var otuTop10 = sampledata.samples[0].otu_ids.slice(0, 10).reverse();
+        var sampleValues = sampledata.samples[0].sample_values.slice(0,10).reverse();
+        var labels = sampledata.samples[0].otu_labels.slice(0,10);
+        // create variable with corrected id format
+        var otuIds = otuTop10.map(id => "OTU " + id);
+        // create trace for bar chart
+        var trace1 = {
+            x: sampleValues,
+            y: otuIds,
+            text: labels,
+            marker: {
+            color: 'blue'},
+            type: "bar",
+            orientation: "h"
+        };
+        // create data variable for bar chart
+        var data = [trace1];
+
+        // create layout for bar chart
+        var layout = {
+            title: "Top 10 Operation Taxinomic Units",
+            yaxis: {
+                tickmode: "linear"
+            },
+            margin: {
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 30
+            }
+        };
+    // create the bar plot
+    Plotly.newPlot("bar", data, layout);
+
     })
-    createcharts(sample.names[0])
-    })
-    function optionChanged (value){
-    
-    createcharts(value)
-    }
+}
