fe80fba475a7a6403bc3dfdc9d08cc931e606aa6
Kirk Kornegay <kkornegay1@gmail.com>
2021-01-06 17:58:29 -0500

    working on gauge chart

---
+++ static/js/bonus.js
@@ -0,0 +1,33 @@
+function plotGaugeChart() {
+
+    var demoPanel = d3.select("#sample-metadata");
+    var gaugeValue = demoPanel.text(wfreq).property("value");
+    var data = [
+        {
+          domain: { x: [0, 1], y: [0, 1] },
+          value: gaugeValue,
+          title: { text: "Belly Button Washing Frequency" },
+          type: "indicator",
+          mode: "gauge+number",
+          gauge: {
+            axis: { range: [null, 9] },
+            steps: [
+              { range: [0, 1], color: "lime" },
+              { range: [1, 2], color: "lightseagreen" },
+              { range: [2, 3], color: "lawngreen" },
+              { range: [3, 4], color: "olive" },
+              { range: [4, 5], color: "darkseagreen" },
+              { range: [5, 6], color: "forestgreen" },
+              { range: [6, 7], color: "green" },
+              { range: [7, 8], color: "limegreen" },
+              { range: [8, 9], color: "seagreen" }
+            ],
+          }
+        }
+      ];
+      
+      var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+      Plotly.newPlot('gauge', data, layout);
+
+}
+plotGaugeChart();
